163df2a1501f8eeb6eb9151df74a67bc165a2c5f
blackstarzck <bucheongosok@gmail.com>
2024-05-16 16:16:16 +0900

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<plotly-plot [data]="graph.data" [layout]="graph.layout"></plotly-plot>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  graph: any = {
+    data: [],
+    layout: {width: 300, height: 300, title: 'A Fancy Plot'}
+};
+
+  ngAfterViewInit(){
+    let x: any = [];
+    let y: any = [];
+    let z: any = [];
+
+    for(let i=0;i<10;i++) {
+      var a_ = Math.random(); 
+      x.push(a_);
+      
+      var b_ = Math.random(); 
+      y.push(b_);
+      
+      var c_ = Math.random(); 
+      z.push(c_);
+    };
+      
+    this.graph.data = [
+      {
+        opacity:0.8,
+        color:'rgb(300,100,200)',
+        type: 'mesh3d',
+        x,
+        y,
+        z,
+      }
+    ];
+  }
 }
